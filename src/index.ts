fa091ac9912eed736c1eca05b388b61b8ac8bf15
awave1 <artgolovin96@gmail.com>
2021-02-22 19:16:23 -0700

    implement ping

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('yo');
+import * as dotenv from 'dotenv';
+import { Client } from 'discord.js';
+
+dotenv.config();
+
+async function start() {
+  const client = new Client();
+
+  client.on('message', (message) => {
+    if (message.content === 'ping') {
+      message.reply('pong');
+    }
+  });
+
+  try {
+    await client.login(process.env.DISCORD_TOKEN);
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+start();
---
+++ src/types/global.d.ts
@@ -0,0 +1,7 @@
+namespace NodeJS {
+  export interface ProcessEnv {
+    DISCORD_CLIENT_SECRET: string;
+    DISCORD_CLIENT_ID: string;
+    DISCORD_TOKEN: string;
+  }
+}
